e085281731a3559e910962a694bdd22d5724502f
ivankasatkin91 <ivankasatkin91@gmail.com>
2022-10-22 03:15:43 +0300

    release 1.00: Basic Spring functions

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = applicationContext.getBean("helloworld",HelloWorld.class);
+
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat",Cat.class);
+
+        // by default equals() execution is identical to '=='
+        System.out.println(bean.equals(bean2));
+        System.out.println(cat.equals(cat2));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+
+    private String voice;
+
+    public String getName() {
+        return voice;
+    }
+
+    public void setVoice (String voice) {
+        this.voice = voice;
+    }
+
+}
